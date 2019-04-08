cc1912f9b85e6039d6da3a0b037492d65e740a70
Deyan Bozhilov <dodobrat98@gmail.com>
2019-04-08 15:58:52 +0300

    removed backend files for now, focusing on front-end. Almost finished with main layout

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,3 +4,11 @@
 window.Popper = require('popper.js');
 require('bootstrap/dist/js/bootstrap.js');
 global.$ = global.jQuery = require('jquery');
+
+
+// -----------------------------------------
+//             MOBILE NAV TOGGLER
+// -----------------------------------------
+$("#mobile-nav-toggler").click(function(){
+    $('#hamburger').toggleClass('is-active');
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,122 @@
 </head>
 <body>
 
+<div class="nav-container">
+    <nav class="main-nav">
+        <div class="nav-logo-container">
+            <a href="#" class="nav-logo">
+asdasd
+                <img src="#" alt="">
+            </a>
+        </div>
+        <div class="nav-links-container">
+            <a data-toggle="collapse" href="#secondary-nav" role="button" aria-expanded="true" aria-controls="secondary-nav" class="nav-link">Home</a>
+            <a href="#" class="nav-link">Team</a>
+            <a href="#" class="nav-link">Contact</a>
+            <a href="#" class="nav-link">EN</a>
+            <a href="#" class="nav-link">FR</a>
+        </div>
+    </nav>
+    <div class="secondary-nav">
+        <div class="collapse show" id="secondary-nav">
+            <a href="#" class="nav-link">Architecture</a>
+            <a href="#" class="nav-link">Real Estate</a>
+        </div>
+    </div>
+</div>
+
+<div class="mobile-nav-container">
+    <nav class="navbar navbar-expand-xl navbar-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler collapsed"
+                id="mobile-nav-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarExpand"
+                aria-controls="navbarExpand"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <div id="hamburger">
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarExpand">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="collapse" href="#homeOptions" role="button" aria-expanded="false" aria-controls="homeOptions">Home</a>
+                </li>
+                <div class="collapse mx-n3" id="homeOptions">
+                    <div class="home-options px-3">
+                        <ul class="navbar-nav mr-auto my-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Architecture</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Real Estate</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">EN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FR</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
 @yield('content')
+
+<footer>
+    <div class="content-container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 footer-field">
+                <h4 class="footer-company-name">{{ config('app.name', 'Mirage Visualisation') }}.</h4>
+                <p class="copyright mb-0 mt-3">
+                    &copy; {{ date('Y') }} {{ config('app.name', 'Mirage Visualisation') }}.
+                </p>
+                <p class="copyright-second">
+                    All rights reserved.
+                </p>
+            </div>
+            <div class="col-lg-4 col-md-12 footer-field">
+                <p class="footer-email mb-0">contact@miragevisualisation.com</p>
+                <p class="footer-phone">+359897521652</p>
+            </div>
+            <div class="col-lg-3 col-md-12 footer-field">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 footer-field">
+                        <a href="#" class="footer-link">Home</a>
+                        <a href="#" class="footer-link">The team</a>
+                        <a href="#" class="footer-link">Contact</a>
+                    </div>
+                    <div class="col-lg-6 col-md-12 footer-field">
+                        <a href="#" class="footer-link">Architecture</a>
+                        <a href="#" class="footer-link">Real Estate</a>
+                        <a href="#" class="footer-link">Projects</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 col-md-12 footer-field mt-lg-0 mt-3">
+                <a href="#" class="footer-social-link mb-0">Facebook</a>
+                <a href="#" class="footer-social-link mb-0">Instagram</a>
+                <a href="#" class="footer-social-link mb-0">Pinterest</a>
+                <a href="#" class="footer-social-link mb-0">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+</footer>
 
 <script src="{{ mix('/js/app.js') }}"></script>
 </body>
