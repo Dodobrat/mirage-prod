b0bbaa3847adf472ac0240b34ca64c5405e76d44
Deyan Bozhilov <dodobrat98@gmail.com>
2019-05-14 15:47:14 +0300

    hidden page under construction message

--- app/Modules/Workflow/Resources/Views/boxes/workflow_content.blade.php
+++ app/Modules/Workflow/Resources/Views/boxes/workflow_content.blade.php
@@ -1,5 +1,17 @@
 @if(!empty($workflow))
 
-    <h1>{{ $workflow->title }}</h1>
+    <div class="container my-5 py-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <div class="card text-white bg-danger mb-3">
+                    <div class="card-header text-center">{{ $workflow->title }}</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Under Construction!</h5>
+                        <p class="card-text">Sorry for the inconvenience</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endif
